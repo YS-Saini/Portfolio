11e9ba549197f81add7d72282da91aa8697fb8b2
Yuvraj Singh <yssaini2004@gmail.com>
2025-07-24 02:58:25 +0530

    fully removed footer section

--- developerFolio/src/components/footer/Footer.js
+++ developerFolio/src/components/footer/Footer.js
@@ -8,16 +8,7 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a
-            href="https://github.com/saadpasta/developerFolio"
-            target="_blank"
-            rel="noreferrer"
-          >
-            developerFolio
-          </a>
-        </p>
+        {/* Footer attribution removed as per user request */}
       </div>
     </Fade>
   );
